64a05413cfa02bbd6d73b746e58b90aad970846d
sufian-utm <sufian.utm@gmail.com>
2025-04-10 08:32:23 +0800

    Update Simulation.py

--- Bearing_defect_simulation/DES/Simulation.py
+++ Bearing_defect_simulation/DES/Simulation.py
@@ -114,8 +114,11 @@ class Simulation(object):
             ax.set_xlabel("Freq (Hz)")
             ax.set_ylabel("Amplitude")
             st.pyplot(fig)
-            return np.linspace(0, self.m_acquisition.m_duration, self.m_acquisition.m_waveform_len), \
-                   self.m_acquisition.m_waveform
+
+            time_axis = np.linspace(0, self.m_acquisition.m_duration, self.m_acquisition.m_waveform_len)
+            amplitude = self.m_acquisition.m_waveform
+            return time_axis, amplitude 
+
         else:
             st.error("Err: Unknown format in get_results()")
 
